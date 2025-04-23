2d41a4da242796c490c32c32d6b6e88732a2ff13
Lennart Schedin <lennart.schedin@gmail.com>
2025-04-23 13:28:54 +0200

    cleanup

--- todo-app/frontend/src/setupProxy.js
+++ todo-app/frontend/src/setupProxy.js
@@ -1,8 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  console.log("Start of setupProxy");
-
   app.use(
     '/api',
     createProxyMiddleware({
