ff2bf56c95baffd2988c2882f1f9e5e25926f0e5
Lennart Schedin <lennart.schedin@gmail.com>
2025-04-23 15:44:40 +0200

    import org

--- todo-app/backend/src/main/kotlin/se/moshicon/klerkframework/todo_app/Application.kt
+++ todo-app/backend/src/main/kotlin/se/moshicon/klerkframework/todo_app/Application.kt
@@ -4,8 +4,8 @@ import dev.klerkframework.klerk.Klerk
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.server.application.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
 
 import kotlinx.coroutines.runBlocking
 
