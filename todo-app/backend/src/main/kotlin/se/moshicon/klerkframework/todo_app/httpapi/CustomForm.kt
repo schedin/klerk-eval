6e75413eafaeeff25e5d8f1f5b67e5bab6d9e683
Lennart Schedin <lennart.schedin@gmail.com>
2025-04-25 10:28:11 +0200

    save before refactor

--- todo-app/backend/src/main/kotlin/se/moshicon/klerkframework/todo_app/httpapi/CustomForm.kt
+++ todo-app/backend/src/main/kotlin/se/moshicon/klerkframework/todo_app/httpapi/CustomForm.kt
@@ -6,13 +6,33 @@ import dev.klerkframework.web.EventFormTemplate
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import dev.klerkframework.klerk.misc.EventParameters
+import dev.klerkframework.web.ParseResult
 import io.ktor.server.html.*
 import io.ktor.server.routing.*
 import kotlinx.html.body
 import se.moshicon.klerkframework.todo_app.*
 import io.ktor.server.routing.get
 
+class CustomForm {
+    fun initCreateTodoTemplate(klerk: Klerk<Ctx, Data>): EventFormTemplate<CreateTodoParams, Ctx> {
+        val createTodoTemplate = EventFormTemplate(
+            EventWithParameters(
+                CreateTodo.id,
+                EventParameters(CreateTodoParams::class)
+            ), klerk, "create",
+        ) {
+            text(CreateTodoParams::title)
+            text(CreateTodoParams::description)
+        }
+        return createTodoTemplate
+    }
+    companion object {
+    }
+}
+
 fun registerCustomRoutes(klerk: Klerk<Ctx, Data>): Route.() -> Unit = {
+
+
     get("/createForm") {
         showCreateTodoForm(call, klerk)
     }
@@ -23,16 +43,15 @@ fun registerCustomRoutes(klerk: Klerk<Ctx, Data>): Route.() -> Unit = {
 
 suspend fun handleCreateTodoFormPost(call: ApplicationCall, klerk: Klerk<Ctx, Data>) {
     call.respond("TODO create not implemented")
-
-//    when (val result = createAuthorFormTemplate.parse(call)) {
-//        is Invalid -> EventFormTemplate.respondInvalid(result, call)
-//        is DryRun -> // TODO: describe what to do here
-//        is Parsed -> // the parameters are now available in result.params
-//    }
+    when (val result = createTodoTemplate.parse(call)) {
+        is ParseResult.Invalid -> EventFormTemplate.respondInvalid(result, call)
+        is ParseResult.DryRun -> println("TODO: describe what to do here")
+        is ParseResult.Parsed -> println(result.params)
+    }
 }
 
 suspend fun showCreateTodoForm(call: ApplicationCall, klerk: Klerk<Ctx, Data>) {
-    val template = initCreateTodoTemplate(klerk)
+    val template = createTodoTemplate(klerk)
     val context = call.context(klerk)
     val createTodoForm = klerk.read(context) {
         template.build(call, null, this, translator = context.translator)
@@ -45,18 +64,8 @@ suspend fun showCreateTodoForm(call: ApplicationCall, klerk: Klerk<Ctx, Data>) {
     }
 }
 
-fun initCreateTodoTemplate(klerk: Klerk<Ctx, Data>): EventFormTemplate<CreateTodoParams, Ctx> {
-    val createTodoTemplate = EventFormTemplate(
-        EventWithParameters(
-            CreateTodo.id,
-            EventParameters(CreateTodoParams::class)
-        ), klerk, "create",
-    ) {
-        text(CreateTodoParams::title)
-        text(CreateTodoParams::description)
-    }
-    return createTodoTemplate
-}
+
+
 
 
 
