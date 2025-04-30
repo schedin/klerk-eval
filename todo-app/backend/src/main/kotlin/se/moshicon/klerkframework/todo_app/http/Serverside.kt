46d828d666bf11758b1aa7ecb42d7108983d20f4
Lennart Schedin <lennart.schedin@gmail.com>
2025-04-30 10:11:03 +0200

    More /serverside

--- todo-app/backend/src/main/kotlin/se/moshicon/klerkframework/todo_app/http/Serverside.kt
+++ todo-app/backend/src/main/kotlin/se/moshicon/klerkframework/todo_app/http/Serverside.kt
@@ -1,21 +1,54 @@
 package se.moshicon.klerkframework.todo_app.http
 
+import dev.klerkframework.klerk.EventWithParameters
 import dev.klerkframework.klerk.Klerk
+import dev.klerkframework.klerk.misc.EventParameters
+import dev.klerkframework.web.EventFormTemplate
 import io.ktor.server.application.*
+import io.ktor.server.html.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.html.body
 import se.moshicon.klerkframework.todo_app.Ctx
 import se.moshicon.klerkframework.todo_app.Data
+import se.moshicon.klerkframework.todo_app.notes.CreateTodo
+import se.moshicon.klerkframework.todo_app.notes.CreateTodoParams
 
+
+object FormTemplates {
+    lateinit var createTodoTemplate: EventFormTemplate<CreateTodoParams, Ctx>
+        private set
+
+    fun init(klerk: Klerk<Ctx, Data>) {
+        createTodoTemplate = EventFormTemplate(
+            EventWithParameters(
+                CreateTodo.id,
+                EventParameters(CreateTodoParams::class)
+            ),
+            klerk, "create",
+        ) {
+            text(CreateTodoParams::title)
+            text(CreateTodoParams::description)
+        }
+    }
+}
 
 fun registerServersideRoutes(klerk: Klerk<Ctx, Data>): Route.() -> Unit = {
-    // Initialize the template when routes are registered
-//    TodoFormTemplate.createTodoTemplate(klerk)
+    FormTemplates.init(klerk)
 
     get("/{...}") { indexPage(call, klerk) }
 }
 
 suspend fun indexPage(call: ApplicationCall, klerk: Klerk<Ctx, Data>) {
-    call.respond("TODO for slash")
+    val context = call.context(klerk)
+    val createTodoForm = klerk.read(context) {
+        FormTemplates.createTodoTemplate.build(call, null, this, translator = context.translator)
+    }
+
+    call.respondHtml {
+        body {
+            createTodoForm.render(this)
+        }
+    }
 }
 
