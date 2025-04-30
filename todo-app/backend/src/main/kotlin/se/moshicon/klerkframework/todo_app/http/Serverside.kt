7e0ec89f40930752e4c66d386f1ee6e40768a0d5
Lennart Schedin <lennart.schedin@gmail.com>
2025-04-30 13:38:34 +0200

    handing create todo

--- todo-app/backend/src/main/kotlin/se/moshicon/klerkframework/todo_app/http/Serverside.kt
+++ todo-app/backend/src/main/kotlin/se/moshicon/klerkframework/todo_app/http/Serverside.kt
@@ -1,17 +1,26 @@
 package se.moshicon.klerkframework.todo_app.http
 
+import dev.klerkframework.klerk.CommandResult
 import dev.klerkframework.klerk.EventWithParameters
 import dev.klerkframework.klerk.Klerk
+import dev.klerkframework.klerk.command.Command
+import dev.klerkframework.klerk.command.ProcessingOptions
 import dev.klerkframework.klerk.misc.EventParameters
 import dev.klerkframework.web.EventFormTemplate
+import dev.klerkframework.web.ParseResult
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.html.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.html.body
 import se.moshicon.klerkframework.todo_app.Ctx
 import se.moshicon.klerkframework.todo_app.Data
 import se.moshicon.klerkframework.todo_app.notes.CreateTodo
 import se.moshicon.klerkframework.todo_app.notes.CreateTodoParams
+import se.moshicon.klerkframework.todo_app.notes.TodoDescription
+import se.moshicon.klerkframework.todo_app.notes.TodoTitle
+import se.moshicon.klerkframework.todo_app.users.UserName
 
 
 object FormTemplates {
@@ -24,7 +33,7 @@ object FormTemplates {
                 CreateTodo.id,
                 EventParameters(CreateTodoParams::class)
             ),
-            klerk, "create",
+            klerk, "todos",
         ) {
             text(CreateTodoParams::title)
             text(CreateTodoParams::description)
@@ -37,12 +46,48 @@ object FormTemplates {
 fun registerServersideRoutes(klerk: Klerk<Ctx, Data>): Route.() -> Unit = {
     FormTemplates.init(klerk)
     get("/{...}") { indexPage(call, klerk) }
+    post("/todos") {
+        handleCreateTodo(call, klerk)
+    }
+}
+
+suspend fun handleCreateTodo(call: ApplicationCall, klerk: Klerk<Ctx, Data>) {
+    when (val result = FormTemplates.createTodoTemplate.parse(call)) {
+        is ParseResult.Invalid -> EventFormTemplate.respondInvalid(result, call)
+        is ParseResult.DryRun -> println("TODO: describe what to do here")
+        is ParseResult.Parsed -> {
+            println("ParseResult.Parsed")
+            val context = call.context(klerk)
+            val command = Command(
+                event = CreateTodo,
+                model = null,
+                params = result.params
+            )
+            when(val commandResult = klerk.handle(command, context, ProcessingOptions(result.key))) {
+                is CommandResult.Failure -> {
+                    call.respond(HttpStatusCode.BadRequest, commandResult.problem.toString())
+                }
+                is CommandResult.Success -> {
+                    val createdTodo = klerk.read(context) {
+                        get(commandResult.primaryModel!!)
+                    }
+                    call.respondRedirect("/")
+                }
+            }
+        }
+    }
 }
 
 suspend fun indexPage(call: ApplicationCall, klerk: Klerk<Ctx, Data>) {
     val context = call.context(klerk)
+    val initialValues = CreateTodoParams(
+        title = TodoTitle(""),
+        description = TodoDescription(""),
+        username = UserName("Alice"),
+    )
+
     val createTodoForm = klerk.read(context) {
-        FormTemplates.createTodoTemplate.build(call, null, this, translator = context.translator)
+        FormTemplates.createTodoTemplate.build(call, initialValues, this, translator = context.translator)
     }
 
     call.respondHtml {
@@ -51,4 +96,3 @@ suspend fun indexPage(call: ApplicationCall, klerk: Klerk<Ctx, Data>) {
         }
     }
 }
-
